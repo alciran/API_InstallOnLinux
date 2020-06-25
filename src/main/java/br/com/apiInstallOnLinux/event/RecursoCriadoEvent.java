881c681f6c6e9b9cc594062f4db1a5afc95d0ff3
Alciran <alciranfranco@gmail.com>
2020-06-25 14:50:53 -0300

    Preparação do ambiente para iniciar programação da API

---
+++ src/main/java/br/com/apiInstallOnLinux/event/RecursoCriadoEvent.java
@@ -0,0 +1,32 @@
+package br.com.apiInstallOnLinux.event;
+
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.context.ApplicationEvent;
+
+/**
+ *
+ * @author alciran
+ */
+public class RecursoCriadoEvent extends ApplicationEvent {
+    private static final long serialVersionUID = 1L;
+    
+    private HttpServletResponse response;
+    private Long id;
+    
+    public RecursoCriadoEvent(Object source, HttpServletResponse response, Long id) {
+		super(source);
+		this.response = response;
+		this.id = id;
+		
+    }
+    
+    public HttpServletResponse getResponse() {
+		return response;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    
+    
+}
